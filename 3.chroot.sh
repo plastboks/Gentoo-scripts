a47b3df5268f9833504de2d9e8c7ef5d46eb351f
Alexander Skjolden <alex@plastboks.net>
2015-11-20 20:40:28 +0100

    various changes to 3.chroot.sh
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- 3.chroot.sh
+++ 3.chroot.sh
@@ -2,19 +2,25 @@
 
 emerge-webrsync
 emerge --sync
+emerge vim
 
-emerge vim \
-        cryptsetup \
-        app-admin/syslog-ng \
-        sys-process/cronie \
-        net-misc/netifrc \
-        sys-apps/mlocate \
-        net-misc/dhcpcd \
-        sys-kernel/gentoo-sources \
-        sys-kernel/linux-firmware \
-        sys-boot/grub
+echo '#USE="bindist mmx sse sse2 libkms xa X xkb gtk xcb xft dir sqlite apng crypt"' >> /etc/portage/make.conf
+vi /etc/portage/make.conf
 
 eselect profile set 2
+
+emerge sys-fs/cryptsetup\
+       app-admin/syslog-ng \
+       sys-process/cronie \
+       net-misc/netifrc \
+       sys-apps/mlocate \
+       net-misc/dhcpcd \
+       sys-kernel/linux-firmware \
+       sys-boot/grub \
+       sys-apps/pciutils \
+       app-portage/eix
+
+eix-update
 
 rc-update add syslog-ng default
 rc-update add cronie default
@@ -22,15 +28,16 @@ rc-update add sshd default
 
 echo "Europe/Oslo" > /etc/timezone
 emerge --config sys-libs/timezone-data
-
-# cd /usr/src/linux
-# make menuconfig
-# make && make modules_install
-# make install
-
+  
+echo sys-kernel/gentoo-sources >> /etc/portage/package.keywords
+emerge --ask sys-kernel/gentoo-sources
+cd /usr/src/linux
+make menuconfig
+make && make modules_install
+make install
 
 mkdir -p /boot/efi/boot
 cp /boot/vmlinuz-* /boot/efi/boot/bootx64.efi
 
 emerge --ask genkernel
-genkernel --lvm --luks --install initramfs
+genkernel --lvm --luks --mdadm --install initramfs
