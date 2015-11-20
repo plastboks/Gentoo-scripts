026485f9bdc78135e720a9ec6e0e5b742aa91566
Alexander Skjolden <alex@plastboks.net>
2015-11-20 20:51:00 +0100

    updated readme
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- 3.chroot.sh
+++ 3.chroot.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# - Sync emerge
+
 emerge-webrsync
 emerge --sync
 emerge vim
 
+# - Edit use flags
+
 echo '#USE="bindist mmx sse sse2 libkms xa X xkb gtk xcb xft dir sqlite apng crypt"' >> /etc/portage/make.conf
 vi /etc/portage/make.conf
 
+# - Set profile
+
 eselect profile set 2
+
+# - Install some software
 
 emerge sys-fs/cryptsetup\
        app-admin/syslog-ng \
@@ -22,19 +30,30 @@ emerge sys-fs/cryptsetup\
 
 eix-update
 
+# - Add software to boot
 rc-update add syslog-ng default
 rc-update add cronie default
 rc-update add sshd default
 
+# - Set timezone and locale
+
 echo "Europe/Oslo" > /etc/timezone
 emerge --config sys-libs/timezone-data
-  
+
+locale-gen
+eselect locale set 5
+
+env-update && source /etc/profile
+
+# - Install kernel
 echo sys-kernel/gentoo-sources >> /etc/portage/package.keywords
 emerge --ask sys-kernel/gentoo-sources
 cd /usr/src/linux
 make menuconfig
 make && make modules_install
 make install
+
+# - Install boot loader
 
 mkdir -p /boot/efi/boot
 cp /boot/vmlinuz-* /boot/efi/boot/bootx64.efi
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 Gentoo Scripts
 ==============
 
-This is just a small repository for me to keep the installation process of Gentoo Linux as quick as possible. There is nothing here meant to be taken seriously.
+This is just a small repository for me to keep the installation process of Gentoo Linux as quick as possible.
+There is nothing here meant to be taken seriously.
+
+Links
+=====
+
+[0.partition.sh](0.partition.sh) - [Web resource](https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Disks)
+[1.stage3.sh](1.stage3.sh) - [Web resource](https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Stage)
+[2.install.base.sh](2.install.base.sh) - [Web resource](https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Base)
+[3.chroot.sh](3.chroot.sh) - [Web resource](https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Kernel)
